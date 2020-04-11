cb4a037ca648e145ce3435b3b9ec66b6cd37c638
Matteo Oldani <matteoldani@Matteos-iMac.fritz.box>
2020-04-11 17:41:23 +0200

    'first commit'

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import logo from "./assets/logo.png";
+import { useFonts } from "@use-expo/font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Rubik": require("./assets/fonts/Rubik/Rubik-Bold.ttf"),
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={{ width: 200, height: 128 }} />
+     { fontsLoaded ? <Text
+        style={{ color: "#000", fontSize: 52, fontFamily: "Rubik" }}
+      >
+        ./bin
+      </Text> : null}
     </View>
   );
 }
@@ -12,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "whitesmoke",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
